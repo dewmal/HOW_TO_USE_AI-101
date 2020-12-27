47efd1689dfb16da196554a8f007ece2b6e406c4
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-12-27 16:27:55 +0530

    Final version

--- android/app/src/main/kotlin/com/example/use_ai_to_detect_offensive_words/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.use_ai_to_detect_offensive_words
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/note.dart
+++
@@ -1,8 +0,0 @@
-class Note {
-  final int id;
-  final String name;
-  final String text;
-  final bool isOffensive; // 1 for offensive text 0 for non offsensive text
-
-  Note(this.id, this.name, this.text, this.isOffensive);
-}
